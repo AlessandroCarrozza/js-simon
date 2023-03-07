a91b5c85e4d3c6a4f640173a71765080de600b43
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-07 16:56:38 +0100

    Generazione dei numeri unici casuali con tempo

---
+++ js/main.js
@@ -0,0 +1,72 @@
+
+// collegamenti al DOM
+const numbersDom = document.getElementById("numbers");
+const secondsDom = document.getElementById("seconds");
+const titleSecondsDom = document.getElementById("title-seconds");
+
+let randomNumbersList = [];
+
+
+showRandomNumbers(0, 5, randomNumbersList);
+console.log(randomNumbersList);
+
+
+
+
+
+
+
+
+// function per mostrare i numeri casuali a schermo
+function showRandomNumbers (min, max, yourList) {
+    for (let i = min; i < max; i++) {
+    
+        const randomNumber = (generateUniqueRandomNumber(randomNumbersList, 1, 100));
+        yourList.push(randomNumber);
+    
+    }  
+    numbersDom.innerHTML = yourList;
+
+    countDown(3);
+}
+
+function countDown (secondsNumber) {
+    let seconds = secondsNumber;
+
+    const clock = setInterval( function() {
+
+        if (seconds == 0) {
+            clearInterval(clock);
+            titleSecondsDom.innerHTML = "Tempo scaduto";
+            numbersDom.innerHTML = "";
+        } else {
+            secondsDom.innerHTML = seconds;
+            seconds--;
+        }
+    }, 1000);
+}
+
+
+
+
+
+
+// functions per la generazione di 5 numeri unici casuali
+function generateUniqueRandomNumber(blacklist, min, max) {
+
+    let isValidNumber = false;
+    let randomNumber;
+
+    while (!isValidNumber) {
+        randomNumber = generateRandomNumber(min, max);
+        if (!blacklist.includes(randomNumber)) {
+            isValidNumber = true;
+        }
+    }
+    return randomNumber;
+}
+
+function generateRandomNumber(min, max) {
+    const number = Math.floor(Math.random() * (max - min + 1)) + min;
+    return number;
+}
